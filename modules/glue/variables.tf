aece7db72600c8680d59148f1f125ea2d0ad21ad
meghanahs1310 <meghana.hs1400@gmail.com>
2025-07-02 14:28:18 +0530

    Update variables.tf

--- modules/glue/variables.tf
+++ modules/glue/variables.tf
@@ -1,2 +1,9 @@
-variable "glue_role_arn" { type = string }
-variable "s3_input_path" { type = string }
+variable "glue_database_name" {}
+variable "glue_crawler_name" {}
+variable "glue_job_name" {}
+variable "glue_role_arn" {}
+variable "script_path" {}
+variable "s3_input_path" {}
+variable "subnet_ids" {
+  type = list(string)
+}
